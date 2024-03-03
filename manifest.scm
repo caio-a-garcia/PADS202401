eac5c7b020fb5662205a7fbaea113fe69032dba3
Caio Garcia <kaioviski7@gmail.com>
2024-03-03 18:13:49 -0300

    Initial commit

---
+++ manifest.scm
@@ -0,0 +1,12 @@
+(specifications->manifest
+ '("r-minimal"
+   "r-ggplot2"
+   "r-tidyverse"
+   "r-arrow"
+   "r-forcats"
+   "r-naniar"
+
+   "jupyter"
+   "python-pandas"
+   "python-matplotlib"
+   ))
